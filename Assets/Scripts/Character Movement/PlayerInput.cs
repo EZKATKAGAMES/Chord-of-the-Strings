58034d027912c3fbbedf1ddee106d6b3fbe82e56
Ombii <39289530+Ombii@users.noreply.github.com>
2018-05-29 10:37:25 +1000

    Early Development Build: 29/05/2018
    New project

---
+++ Assets/Scripts/Character Movement/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script executes anything not done in the player controller script.
+
+[RequireComponent (typeof (PlayerController))]
+public class PlayerInput : MonoBehaviour
+{
+
+    PlayerController player;
+    Vector2 directionalInput;
+    HorzontalVerticalVelocity directionVel;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        directionVel = GetComponent<HorzontalVerticalVelocity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {               
+        directionalInput = new Vector2(directionVel.horizontalVelocity, directionVel.verticalVelocity);
+        player.SetDirectionalInput(directionalInput);
+        
+        if (Input.GetKeyDown(GameManager.GM.Jump))
+        {
+            player.OnJumpInputDown();
+        }
+
+        if (Input.GetKeyUp(GameManager.GM.Jump))
+        {
+            player.OnJumpInputUp();
+        }
+    }
+
+   
+}
