a27c22117c789636e111447b683c5e9ed650bf5e
HarryLottey <harry.lottey@gmail.com>
2018-05-29 22:28:44 +1000

    Early Development Build: 29/05/2018
    Jump, half complete.

---
+++ Assets/Scripts/Character Movement/HorizontalVerticalVelocity.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalVerticalVelocity : MonoBehaviour
+{
+    // The purpose of this script is to controll values which are used as a multiplier to control our velocity.
+    // TODO: Make it better.
+
+
+    public float horizontalVelocity;
+    public float verticalVelocity;
+    public float hVelocityPositiveLimit = 1; // By defualt we cannot bypass this multiplier for calculating our speed.
+    public float vVelocityPositiveLimit = 1;
+    public bool unCapVelocity;
+
+    private void Update()
+    {
+        MoveRightLeft();
+        MoveUpDown();
+        VelocityCap();
+    }
+
+   
+
+    private void Awake()
+    {
+        
+    }
+
+    void VelocityCap()
+    {
+        if (unCapVelocity == false)
+        {
+            if (horizontalVelocity > hVelocityPositiveLimit) // Precausion to stop velocity going outside bounds.
+            {
+                horizontalVelocity = hVelocityPositiveLimit;
+                return;
+            }
+
+
+            if (horizontalVelocity < 0 && Input.GetKeyDown(GameManager.GM.Right))
+            {
+                horizontalVelocity = 0;
+            }
+
+            if (horizontalVelocity > 0 && Input.GetKeyDown(GameManager.GM.Left))
+            {
+                horizontalVelocity = 0;
+            }
+
+            if (verticalVelocity > vVelocityPositiveLimit)
+            {
+                verticalVelocity = vVelocityPositiveLimit;
+                return;
+            }
+
+            if (verticalVelocity < 0 && Input.GetKeyDown(GameManager.GM.Right))
+            {
+                verticalVelocity = 0;
+            }
+
+            if (verticalVelocity > 0 && Input.GetKeyDown(GameManager.GM.Left))
+            {
+                verticalVelocity = 0;
+            }
+
+        }
+    }
+
+    void MoveRightLeft()
+    {
+        #region Horizontal
+
+        if (Input.GetKey(GameManager.GM.Right))
+        {
+            horizontalVelocity += 0.1f;
+        }
+        else if (Input.GetKey(GameManager.GM.Left))
+        {
+            horizontalVelocity -= 0.1f;
+        }
+        else // Reset to zero when no input is recieved.
+        {
+            if (horizontalVelocity == 0)
+                return; // dont run the code if we are stationary!
+            horizontalVelocity = Mathf.Lerp(horizontalVelocity, 0, 1f);
+        }
+        #endregion
+        // Clamp velocity to a reasonable limit.
+        horizontalVelocity = Mathf.Clamp(horizontalVelocity, -1, 1);
+    }
+
+    void MoveUpDown()
+    {
+        #region Vertical
+        if (Input.GetKey(GameManager.GM.Up))
+        {
+            verticalVelocity += 0.1f;
+        }
+        else if (Input.GetKey(GameManager.GM.Down))
+        {
+            verticalVelocity -= 0.1f;
+        }
+        else // Reset to zero when no input is recieved.
+        {
+            if (verticalVelocity == 0)
+                return; // dont run the code if we are stationary!
+            verticalVelocity = Mathf.Lerp(verticalVelocity, 0, 1f);
+        }
+
+        #endregion
+
+        verticalVelocity = Mathf.Clamp(verticalVelocity, -1, 1);
+    }
+}
--- Assets/Scripts/Character Movement/PlayerCharacter.cs
+++ Assets/Scripts/Character Movement/PlayerCharacter.cs
@@ -5,33 +5,62 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     //Public Variables
+    [Header("Movement")]
     public float moveSpeed = 0.0f;
+    public float timeToJumpApex;
+    public float maxJumpHeight;
+
     public Transform rotateTarget;
     public Vector3 mousePos;
     public float cameraRayLength = 0.0f;
     public float characterRotationAmount = 0.0f;
+
     //Private Variables
     private Rigidbody myRB;
+    private float jumpVelocity;
+    public float gravity;
+    public Vector2 velocity;
+    Vector3 input;
 
-    void Start ()
+    void Start()
     {
+        #region Setting up variables
         rotateTarget = GetComponentInChildren<Transform>();
         myRB = GetComponent<Rigidbody>();
+        #endregion
+
+        #region Jump Velocity Variables
+        velocity = myRB.velocity; // Rigid body current velocity
+        gravity = (2 * maxJumpHeight / Mathf.Pow(timeToJumpApex,2)); // Set gravity;
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex; // Set jumpVelocity
+        #endregion
+
     }
-	
-	void FixedUpdate ()
+
+    private void Update()
     {
+        ApplyGravity();
+    }
+
+    void FixedUpdate()
+    {
+        #region Movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(moveHorizontal, 0, moveVertical);
+        input = new Vector3(moveHorizontal, 0, moveVertical);
 
-		if(Input.GetAxis("Horizontal") != 0|| (Input.GetAxis("Vertical") != 0))
+        if (Input.GetAxis("Horizontal") != 0 || (Input.GetAxis("Vertical") != 0))
         {
             print("TestMovement");
-            myRB.AddForce(movementDirection * moveSpeed, ForceMode.VelocityChange);
+            myRB.AddForce(input * moveSpeed, ForceMode.VelocityChange);
         }
 
+        
+
+        #endregion
+
+        #region Mouse Aiming & LookDirection
         Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit floorHit;
 
@@ -42,11 +71,42 @@ public class PlayerCharacter : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, aimRotation, Time.deltaTime * characterRotationAmount);
             transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
         }
+        #endregion
+
+        
+
 
     }
 
-    private void Update()
+    private void OnCollisionStay(Collision collision)
     {
+        if(collision.collider.gameObject.layer == 9) // temporary ground check do proper later
+        {
+            velocity.y = 0; // Prevent gravity accumulations MOVE THIS TO PHYSICS.cast CHECK
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+    }
+
+    void ApplyGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
         
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = jumpVelocity;
+            myRB.AddForce(new Vector3(0,velocity.y), ForceMode.Impulse); // Apply the velocity values to our rigid body as an impulse force.
+
+        }
+
+       
     }
 }
