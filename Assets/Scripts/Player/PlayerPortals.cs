9b006ebc6f597ac73958e886cba443988d4f1a90
Ombii <39289530+Ombii@users.noreply.github.com>
2018-05-31 13:47:04 +1000

    Early Development Build: 31/05/2018
    Made portals and transitioning between scenes alot easier. Fixed Spawns for the player. Colliders for the portals are up and running.

---
+++ Assets/Scripts/Player/PlayerPortals.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPortals : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        // Portal Back to Earth
+        if (col.tag == "Earth")
+        {
+            SceneManager.LoadScene(0);
+
+            Destroy(gameObject);
+        }
+
+        // Portal Back to HUB
+        if (col.tag == "HUB")
+        {
+            SceneManager.LoadScene(1);
+
+            Destroy(gameObject);
+        }
+
+        // Portal to Tutorial Combat - Puzzles
+        if (col.tag == "TutorialCombat")
+        {
+            SceneManager.LoadScene(2);
+
+            Destroy(gameObject);
+        }
+
+        // Portal to Tutorial Boss Fight
+        if (col.tag == "TutorialBoss")
+        {
+            SceneManager.LoadScene(3);
+
+            Destroy(gameObject);
+        }
+    }
+}
