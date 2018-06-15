aaf085bb0b93e46028b92a3970ab656c92ed3bb3
prettyasianswag <rich_zhanga@outlook.com>
2018-06-15 21:05:53 +1000

    Test Development Build: 15/06/18
    Added hazards and ability 2 puzzles and moving platforms

---
+++ Assets/Scripts/Puzzle/Puzzle 2/PuzzleTwoMovingObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTwoMovingObject : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    private int currentPoint;
+    public float MovementSpeed = 2f;
+
+    void Start()
+    {
+        // First point
+        currentPoint = 0;
+
+        // Starting Position
+        transform.position = Waypoints[currentPoint].position;
+    }
+
+    void Update()
+    {
+        // If it reaches the waypoint
+        if (transform.position == Waypoints[currentPoint].position)
+        {
+            currentPoint++;
+        }
+
+        // If it reaches the last waypoint, return to the beginning
+        if (currentPoint >= Waypoints.Length)
+        {
+            currentPoint = 0;
+        }
+
+        // Movement of the object
+        transform.position = Vector3.MoveTowards(transform.position, Waypoints[currentPoint].position, MovementSpeed * Time.deltaTime);
+
+    }
+}
