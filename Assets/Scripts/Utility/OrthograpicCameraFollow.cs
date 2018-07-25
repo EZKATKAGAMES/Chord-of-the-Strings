556b886c844772a87e87e83c4d3bcbbac55bd8bc
Ombii <39289530+Ombii@users.noreply.github.com>
2018-07-25 15:57:43 +1000

    Early Development Build: 25/07/2018
    Progress on camera rotation
    
    maybe 85% complete
    todo: follow smoothing.
    and movement + ability fixes.

--- Assets/Scripts/Utility/OrthograpicCameraFollow.cs
+++ Assets/Scripts/Utility/OrthograpicCameraFollow.cs
@@ -41,6 +41,7 @@ public class OrthograpicCameraFollow : MonoBehaviour
         // Set defaults
         defaultPosition = new Vector3(0, 18, 0);
         defaultRotation = new Vector3(30, 45, 0);
+ 
         // Apply them at start.
         gameObject.transform.position = defaultPosition;
         gameObject.transform.rotation = Quaternion.Euler(defaultRotation);
@@ -50,8 +51,7 @@ public class OrthograpicCameraFollow : MonoBehaviour
     void Update()
     {
         RotateCamera();
-
-
+        SetLerpValues();
         FollowTarget();
     }
 
@@ -59,7 +59,70 @@ public class OrthograpicCameraFollow : MonoBehaviour
     void FollowTarget()
     {
         // Target x, camerazoom Y, target Z
+        // TODO: SMOOTHING
         gameObject.transform.position = new Vector3(target.transform.position.x, cameraZoom, target.transform.position.z);
+    }
+
+    void SetLerpValues()
+    {
+        float v1 = 45;
+        float v2 = -45;
+        float v3 = -140;
+        float v4 = 140;
+
+        if (rotationY == v1)
+        {
+            view2 = view3 = view4 = false;
+            view1 = true;
+            
+        }
+        else if (rotationY == v2)
+        {
+            view1 = view3 = view4 = false;
+            view2 = true;
+
+        }
+        else if (rotationY == v3)
+        {
+            view2 = view1 = view4 = false;
+            view3 = true;
+
+        }
+        else if (rotationY == v4)
+        {
+            view2 = view3 = view1 = false;
+            view4 = true;
+
+        }
+
+        if (view1)
+        {
+
+            view2 = view3 = view4 = false;
+            lerpTargetNegative = v2;
+            lerpTargetPositive = v4;
+        }
+
+        if (view2)
+        {
+            view1 = view3 = view4 = false;
+            lerpTargetNegative = v3;
+            lerpTargetPositive = v1;
+        }
+
+        if (view3)
+        {
+            view1 = view2 = view4 = false;
+            lerpTargetNegative = v4;
+            lerpTargetPositive = v2;
+        }
+
+        if (view4)
+        {
+            view1 = view2 = view3 = false;
+            lerpTargetNegative = v1;
+            lerpTargetPositive = v3;
+        }
     }
 
     void RotateCamera()
@@ -68,31 +131,61 @@ public class OrthograpicCameraFollow : MonoBehaviour
         // Allow rotation when the camera is not currently rotating
         if (Input.GetKeyDown(KeyCode.E) && !cameraRotating)
         {
-            rotationStrength += Time.deltaTime * rotationSpeed;
-            rotationY = Mathf.Lerp(rotationY, lerpTargetPositive, rotationStrength);
+            cameraRotating = true;
+            cameraRotateN = true;
+            cameraRotateP = false;
 
         }
 
-        
+        if (cameraRotateN)
+        {
+            StartCoroutine(RotateProperly());
+            //val = (1-t)*v0 + t* v1
+            rotationY = (1 - rotationStrength) * rotationY + rotationStrength * lerpTargetNegative;
+            
+        }
 
-      
-
+     
         if (Input.GetKeyDown(KeyCode.Q) && !cameraRotating)
         {
-            // Increase rotation speed as it rotates.
-            rotationStrength += Time.deltaTime * rotationSpeed;
-            rotationY = Mathf.Lerp(rotationY, lerpTargetPositive, rotationStrength);
+            cameraRotating = true;
+            cameraRotateP = true;
+            cameraRotateN = false;
         }
 
         if (cameraRotateP)
         {
-           
-
-           
+            StartCoroutine(RotateProperly());
+            // val = (1-t)*v0 + t* v1
+            //rotationY; FINISH
+            
         }
 
         // Apply rotation
         // X is being set to static default value. Y is set to dynamic value we change.
         gameObject.transform.rotation = Quaternion.Euler(new Vector3(defaultRotation.x, rotationY, 0));
+       
+        
+    }
+
+    
+    public static float Lerp(float start, float finish, float percentage)
+{
+    //Make sure percentage is in the range [0.0, 1.0]
+    percentage = Mathf.Clamp01(percentage);
+
+    //(finish-start) is the Vector3 drawn between 'start' and 'finish'
+    float startToFinish = finish - start;
+ 
+    //Multiply it by percentage and set its origin to 'start'
+    return start + startToFinish * percentage;
+}
+
+    IEnumerator RotateProperly()
+    {
+        yield return new WaitForSeconds(1.3f);
+        cameraRotateN = false;
+        cameraRotateP = false;
+        cameraRotating = false;
     }
 }
